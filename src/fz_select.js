6fec0e42b2ff958906e476f62efadff65bcd4695
Patrick Brown <patrickbrowndev@gmail.com>
2015-07-17 15:13:28 -0500

    Finish basic selection logic

--- src/fz_select.js
+++ src/fz_select.js
@@ -28,10 +28,6 @@
  *                        Useful if you have a list like [ {name: '', email: ''} 
  *                        and want to search by name, but return the email
  *
- * fzIncludeNullOption:   Expects boolean
- *                        Set to true if you want a null item prepended to the 
- *                        result list to allow for a blank option
- *
  * fzInitialSearchString  Expects string
  *                        The initial search string for the component
  *
@@ -58,6 +54,7 @@ angular.module( "fzSelect", [] )
 '<div class="fz-search-box" >' +
   '<input ' + 
     'class="fz-search-input" ' +
+    'ng-keydown="onInputKeydown($event)" '+
     'ng-change="searchStringChanged()" ' +
     'ng-model="searchString" />' +
   '<div ' +
@@ -66,10 +63,12 @@ angular.module( "fzSelect", [] )
     '/>' +
   '<div ' +
     'class="fz-results-container" ' +
-    'ng-repeat="filteredItem in filteredItems" >' +
+    'tabindex=0 '+
+    'ng-keydown="onResultsKeydown($event)" >' +
 
     '<div ' +
       'ng-class="{\'fz-selected\': isHighlightedItem(filteredItem)}" ' +
+      'ng-repeat="filteredItem in filteredItems" ' + 
       'class="fz-results-row" >' +
       '{{getResultDisplay(filteredItem)}}' +
     '</div> ' + 
@@ -90,27 +89,101 @@ angular.module( "fzSelect", [] )
         var refreshRate = null;
 
         var asyncRefreshPromise = null;
+
+
         scope.filteredItems = [];
         scope.selectedItem = null;
         scope.searchString = null;
+        highlightedItem = null;
 
         var highlightedItem = null;
 
         scope.isHighlightedItem = function(item){
-          return scope.highlightedItem === item;
+          return highlightedItem === item;
         }
 
         scope.searchStringChanged = function(){
           filterItems();
           orderItems();
-          updateCurrentItem();
         };
 
         var updateNgModel = function(){
           scope.ngModel = scope.selectedItem;
         };
 
-        var updateCurrentItem;
+        var selectItem = function(){
+          scope.selectedItem = highlightedItem;
+          updateNgModel();
+        }
+
+
+
+        var focusSearchBox = function(){
+
+        };
+
+        scope.onResultsKeydown = function($event){
+          if($event != undefined)
+            $event.preventDefault();
+          currentIndex = scope.filteredItems.indexOf(highlightedItem);
+
+          switch($event.keyCode){
+            case 40:
+              if(currentIndex != scope.filteredItems.length-1)
+                highlightedItem = scope.filteredItems[currentIndex + 1];
+              break;
+            case 38:
+              if(currentIndex == 0)
+                focusSearchBox();
+              else
+                highlightedItem = scope.filteredItems[currentIndex - 1];
+              break;
+            case 13:
+              selectItem();
+            default:
+              return;
+          }
+          
+        };
+
+        var focusResults = function(startAtTop, $event){
+          if($event != undefined)
+            $event.preventDefault();
+
+          if(startAtTop){
+            highlightedItem = scope.filteredItems[0];
+          } else { 
+            highlightedItem = 
+              scope.filteredItems[scope.filteredItems.length-1];
+          }
+
+          results = $(element).find('.fz-results-container');
+          if(results && results.focus)
+            results.focus();
+
+        };
+
+        var selectDefaultItem = function(){
+          highlightedItem = scope.filteredItems[0];
+          selectItem();
+        };
+
+        scope.onInputKeydown = function($event){
+          switch($event.keyCode){
+            case 40:
+              
+              focusResults(true, $event);
+              break;
+            case 38:
+              focusResults(false, $event);
+              break;
+            case 13: 
+              selectDefaultItem();
+              break;
+            default:
+              return;
+          }
+        }
 
         //returns true/false if the items match
         var itemsMatch = function(item1, item2){
@@ -121,24 +194,14 @@ angular.module( "fzSelect", [] )
           if(item1 == null || item2 == null)
             return false;
 
-          if(fzMatchAttribute)
-            return item1[fzMatchAttribute] == item2[fzMatchAttribute];
+          if(matchAttribute)
+            return item1[matchAttribute] == item2[matchAttribute];
           else
             return item1 == item2;
         };
 
-        // sets highlightedItem to the first item in the list that matches
-        var setCurrentItem = function(){
-          for(i=0; i<scope.fzSelectItems.length; i++){
-            if(itemsMatch(scope.fzSelectItems[i], scope.ngModel)){
-              highlightedItem = fzSelectItems[i];
-              break;
-            }
-          }
-        };
-
         var orderItems = function(){
-          if(fzMatchAttribute){
+          if(matchAttribute){
             scope.filteredItems = $filter('orderBy')(
               scope.filteredItems, 
               matchAttribute,
@@ -182,12 +245,6 @@ angular.module( "fzSelect", [] )
             refreshRate = DEFAULT_REFRESH_RATE;
 
           if(!isAsync){
-            //if ngModel has an initial value, initialize the component to that value
-            if(!(scope.ngModel == "" && scope.ngModel == null)){
-              setSelectedItem();
-            }
-            selectedItemInitialized = true;
-            listInitialized = true;
           } else {
             startAsyncRefresh();
           }
@@ -220,7 +277,6 @@ angular.module( "fzSelect", [] )
 
         initListeners = function(){
           scope.$watch('fzSelectItems', function(){
-            setCurrentItem();
           });
           scope.$on('$destroy', cleanUp);
         }
